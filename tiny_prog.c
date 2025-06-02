66ceb92e82e7eaeec2b7d99f3ee2276947ca35af
twotoe1 <simkondavitu@gmail.com>
2025-06-02 02:58:05 +0200

    more lessons

---
+++ tiny_prog.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{ 
+	/*
+	 * understanding pointer and arrays in c. proper foundation for memory managment manipulation
+	 */
+	
+	int a; 
+	int* p;
+
+	p = &a;
+
+	printf("address in memo at a %d\n", p);
+} 
