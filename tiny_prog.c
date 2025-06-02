76e4f957329c8ab83c9b79162bd08537b77558ef
twotoe1 <simkondavitu@gmail.com>
2025-06-02 19:47:29 +0200

    updated code from termux-android emulator

--- tiny_prog.c
+++ tiny_prog.c
@@ -3,7 +3,11 @@
 int main()
 { 
 	/*
-	 * understanding pointer and arrays in c. proper foundation for memory managment manipulation
+	 * understanding pointer and arrays in c. proper foundation for memory managment manipulation.
+	short notes taken on the shorthand notations
+	*p same as *(p + i);
+	p same as &a;
+
 	 */
 	
 	int a; 
@@ -12,4 +16,30 @@ int main()
 	p = &a;
 
 	printf("address in memo at a %d\n", p);
+
+	a = 67;
+
+	printf("value in memo address stored in a variable a %d\n", *p);
+
+
+	printf("size of the bytes in memo stored at variable a %d bytes", sizeof(*p));
+	/*
+	 * But now we want to delve more deeper on thi topic and there we want to leverage the power of pointers and arrays.
+	 * The outputs of printf function thats prints two similar addresses in memory storing diffirent interger variables */
+
+	int b = 21;
+	p = &b;
+
+	printf("the address of b variable in memo %d\n", p);
+
+	/*
+	 *The fourth print statement shows how to print the values stored in the b variable, in this case p as a pointer variable.*/
+
+	printf("value stored in b variable and a pointer variable p %d\n", *p);
+
+	printf("size of the bytes in memo stored at b variable b %d bytes", sizeof(*p));
+
+	/*
+	 * The above snippets of code shows a pointer variable p storing two diff interger varibles storing diff integer values and the sizeof() function shows bytes stored in interger variable.
+	 * The next step will explore more on pointer arithmetic..*/
 } 
